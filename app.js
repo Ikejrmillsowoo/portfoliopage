7ded4749e1bf7d51e604b380dba6d0d766482a9d
poppymills2@gmail.com <poppymills2@gmail.com>
2021-04-15 00:14:18 -0400

    updated resume

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('#header');
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector("#header");
 
-console.log(header)
-hamburger.addEventListener('click', () =>{
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+console.log(header);
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
-header.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 250){
-        header.style.backgroundColor = "rgba(31, 30, 30, 0.90)";
-    }else {
-        header.style.backgroundColor = "transparent";
-    }
+header.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "rgba(31, 30, 30, 0.90)";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
 
-menu_item.forEach(item=> {
-    item.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-    })
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
